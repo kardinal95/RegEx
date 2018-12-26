0eb46af54d7e4ad54ebcf684c68db2a2f32caa51
Ivan Ustalov <ustalovi@gmail.com>
2018-12-26 23:44:56 +0500

    Fukken automati

--- run.py
+++ run.py
@@ -3,8 +3,11 @@ from py.parser import RegexParser
 
 def main() -> None:
     # Input and validation
-    temp = '(a.(b|c))*.c.#'
+    temp = '(a|a*|a|a).c.#'
     parser = RegexParser(temp)
+    fsm = parser.fsm
+    print(fsm.restore_re())
+    pass
 
 
 if __name__ == '__main__':
